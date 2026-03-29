7b762286d366cb5089e326d74b5e6998f8be8cda
viniamadeu <viniamadeu@me.com>
2026-03-29 13:22:25 -0300

    Create ui_home.h

---
+++ ui_home.h
@@ -0,0 +1,15 @@
+#ifndef UI_HOME_H
+#define UI_HOME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ui_home_create(void);
+void ui_home_set_status(const char *txt);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /* UI_HOME_H */
